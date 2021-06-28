f8785347ab1f62896ee3762c9efea2193b28dedd
Huzaifa Raghav <windowpane1712@gmail.com>
2021-06-28 20:51:22 +0800

    Sub commit, change folders

---
+++ py_files/test.py
@@ -0,0 +1,10 @@
+def test(x,y):
+    return x
+
+arr = [1,2,3]
+
+for i in arr:
+    print(i)
+
+
+
---
+++ py_files/test2.py
@@ -0,0 +1,8 @@
+arr = []
+
+N = int(input())
+
+for i in range(N):
+    arr.append(input())
+
+print(arr)
